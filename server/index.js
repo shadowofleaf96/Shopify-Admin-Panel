c723b55ce945c456e807a716d22dbdcc906b6b8c
shadowofleaf96 <shadowofleaf96@gmail.com>
2024-06-11 12:43:33 +0100

    fix: add frontend new render link in cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 const port = process.env.PORT || 3000;
-const allowedOrigins = ["http://localhost:4173", "http://localhost:5173"];
+const allowedOrigins = ["http://localhost:4173", "http://localhost:5173", "https://shopify-admin-panel.onrender.com/login"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
