57c3bb6990e5d910a7016fb167e504caf3582443
Shadow Of Leaf <shadowofleaf96@gmail.com>
2024-09-05 01:30:43 +0100

    chores: add some more debugging

--- client/src/components/Utils/ProtectedRoute.jsx
+++ client/src/components/Utils/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 const ProtectedRoute = () => {
   const isAuthenticated = document.cookie.includes('SessionID');
-  console.log(document.cookie.includes('SessionID'))
+  console.log(document.cookie)
   console.log('isAuthenticated', isAuthenticated);
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
